608cee38fd9925504a3d7cf7d272e20daa4a5a37
Ismoil Atajanov <ismail11270@gmail.com>
2020-11-06 09:42:26 +0100

    Assets cleanup & scenes merge

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+
+public class Patrol : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        agent.speed = Random.Range(0.5f, 3);
+        animator.speed = agent.speed*2f;
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+}
---
+++ Assets/Scripts/WeatherSwitchScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeatherSwitchScript : MonoBehaviour
+{
+    public Slider slider;
+    public Text text;
+
+    private int currentWeather;
+    public GameObject[] weatherList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWeather = -1;
+        UpdateWeather();
+        slider.onValueChanged.AddListener(delegate { UpdateWeather(); });
+    }
+
+
+    private void UpdateWeather()
+    {
+        if(currentWeather != -1)
+        {
+            weatherList[currentWeather].SetActive(false);
+        }
+        currentWeather = (int)slider.value;
+        weatherList[currentWeather].SetActive(true);
+        text.text = "Weather Condition #" + (currentWeather + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
